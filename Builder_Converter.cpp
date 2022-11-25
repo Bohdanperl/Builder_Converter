e60d3f760171367ecc7c58364e327c4b77fa7688
bogdanperley <88403935+bogdanperley@users.noreply.github.com>
2022-11-25 21:41:53 +0200

    Update Builder_Converter.cpp

--- Builder_Converter.cpp
+++ Builder_Converter.cpp
@@ -1,18 +1,15 @@
-﻿#include"Builder_Converter.h"
+#include "Builder_Converter.h"
 
-
-Builder& Html_Builder::add_child(std::string tag, std::string val)
-{
-	str_ +=  "<" + tag + ">" + val + R"("<\")" + tag + ">\n";
-	return *this;
+Builder &Html_Builder::add_child(std::string tag, std::string val) {
+  str_ += "<" + tag + ">" + val + R"("<\")" + tag + ">\n";
+  return *this;
 }
 
-std::string Html_Builder::str(){ return str_;}
+std::string Html_Builder::str() { return str_; }
 
-
-Builder& Json_Builder::add_child(std::string tag, std::string val){
-	str_ +=  R"(")" + tag + R"(")" + ":" + val ;
-	return *this;
+Builder &Json_Builder::add_child(std::string tag, std::string val) {
+  str_ += R"(")" + tag + R"(")" + ":" + val;
+  return *this;
 }
 
-std::string Json_Builder::str(){return str_;}
+std::string Json_Builder::str() { return str_; }
